2000b01615b34d2ad9b940aa22858130dc37ef7a
STUDENT_M16 <student_m16@PS-DAL-M16.local>
2020-08-28 15:05:29 -0500

    added text on directory

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -10,47 +10,55 @@ const Directory = () => (
         <a href="https://the-talks.com/interviews/architecture/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/ARCHITECTURE-David-Chipperfield-325x170.jpg" />
         </a>
+        <p>ARCHITECTURE</p>
       </div>
 
       <div>
         <a href="https://the-talks.com/interviews/art/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/ART-Anish-Kapoor-325x170.jpg" />
         </a>
+        <p>ART</p>
       </div>
 
       <div>
         <a href="https://the-talks.com/interviews/fashion/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/FASHION-Yohji-Yamamoto-325x170.jpg" />
         </a>
+        <p>FASHION</p>
       </div>
 
       <div>
         <a href="https://the-talks.com/interviews/film/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/FILM-Jim-Carrey-325x170.jpg" />
         </a>
+        <p>FILM</p>
       </div>
 
       <div>
         <a href="https://the-talks.com/interviews/food/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/FOOD-Massimo-Bottura-325x170.jpg" />
         </a>
+        <p>FOOD</p>
       </div>
 
       <div>
         <a href="https://the-talks.com/interviews/literature/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/LITERATURE-Bret-Easton-325x170.jpg" />
         </a>
+        <p>LITERATURE</p>
       </div>
       <div>
         <a href="https://the-talks.com/interviews/music/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/MUSIC-Alicia-Keys-325x170.jpg" />
         </a>
+        <p>MUSIC</p>
       </div>
 
       <div>
         <a href="https://the-talks.com/interviews/sports/">
           <img src="https://the-talks.com/wp-content/uploads/2019/06/SPORT-Lebron-James-325x170.jpg" />
         </a>
+        <p>SPORTS</p>
       </div>
     </div>
   </section>
